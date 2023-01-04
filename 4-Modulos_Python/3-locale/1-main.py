defbb8a71a5e60d7ac6f5074d929388716df8c0e
Willian <willian.machado.av@gmail.com>
2023-01-04 15:28:51 -0300

    Aula de modulos - locale

---
+++ 4-Modulos_Python/3-locale/1-main.py
@@ -0,0 +1,12 @@
+# locale para internacionalização (tradução)
+# https://docs.python.org/3/library/locale.html
+# https://learn.microsoft.com/fr-fr/powershell/module/international/get-winsystemlocale?view=windowsserver2022-ps&viewFallbackFrom=win10-ps
+# locale -a
+import calendar
+import locale
+
+# locale.setlocale(locale.LC_ALL, '')
+
+print(calendar.calendar(2023))
+
+# print(locale.getlocale())
